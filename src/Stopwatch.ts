f44394379d526f19df076daa159791ef3188b112
Teder-kedr <124440529+Teder-kedr@users.noreply.github.com>
2023-11-01 15:04:28 +0400

    finished Stopwatch

--- src/Stopwatch.ts
+++ src/Stopwatch.ts
@@ -1,29 +1,23 @@
-class Stopwatch {
+export class Stopwatch {
   private readonly PRECISION_MS = 10;
   private intervalId: number | null = null;
   value = 0;
 
-  constructor() {
-    return this;
-  }
+  constructor() {}
 
   get isRunning() {
     return !!this.intervalId;
   }
 
-  get isZero() {
-    return !this.value;
-  }
-
   start() {
-    this.intervalId = setInterval(() => console.log(this.value++), this.PRECISION_MS);
+    this.intervalId = setInterval(() => {
+      this.value++;
+    }, this.PRECISION_MS);
   }
 
   stop() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
+    clearInterval(this.intervalId!);
+    this.intervalId = null;
   }
 
   reset() {
@@ -31,4 +25,59 @@ class Stopwatch {
   }
 }
 
-export default Stopwatch;
+export class StopwatchFace {
+  private readonly REFRESH_RATE_MS = 32;
+  private intervalId: number | null = null;
+  private element: HTMLElement | null = null;
+  sw: Stopwatch;
+
+  constructor(sw: Stopwatch) {
+    this.sw = sw;
+  }
+
+  private get hours() {
+    return Math.floor(this.sw.value / 360000);
+  }
+
+  private get minutes() {
+    const remainingHundredths = this.sw.value % 360000;
+    return Math.floor(remainingHundredths / 6000);
+  }
+
+  private get seconds() {
+    const remainingHundredths = (this.sw.value % 360000) % 6000;
+    return Math.floor(remainingHundredths / 100);
+  }
+
+  private get hundredths() {
+    return ((this.sw.value % 360000) % 6000) % 100;
+  }
+
+  private stringify(value: number) {
+    return value.toString().padStart(2, "0");
+  }
+
+  private get value() {
+    return [this.hours, this.minutes, this.seconds, this.hundredths].map(this.stringify).join(":");
+  }
+
+  mount(el: HTMLElement) {
+    this.element = el;
+    this.element.innerText = this.value;
+  }
+
+  updateOnce() {
+    this.element!.innerText = this.value;
+  }
+
+  watch() {
+    this.intervalId = setInterval(() => {
+      this.updateOnce();
+    }, this.REFRESH_RATE_MS);
+  }
+
+  unwatch() {
+    clearInterval(this.intervalId!);
+    this.intervalId = null;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import "./style.css";
-import Stopwatch from "./Stopwatch";
+import { Stopwatch, StopwatchFace } from "./Stopwatch";
+
+// -------- STOPWATCH --------
+const swStartStopBtn = document.querySelector("#sw-startstop") as HTMLButtonElement;
+const swResetBtn = document.querySelector("#sw-reset") as HTMLButtonElement;
 
 const sw = new Stopwatch();
+const swf = new StopwatchFace(sw);
+swf.mount(document.querySelector("#sw-face")!);
 
-document.querySelector("#sw-startstop")?.addEventListener("click", () => {
-  if (sw.isRunning) {
-    sw.stop();
-  } else {
+swStartStopBtn.addEventListener("click", () => {
+  if (!sw.isRunning) {
     sw.start();
+    swf.watch();
+    swStartStopBtn.innerText = "STOP";
+  } else {
+    sw.stop();
+    swf.unwatch();
+    swStartStopBtn.innerText = "START";
   }
+});
+
+swResetBtn.addEventListener("click", () => {
+  sw.reset();
+  swf.updateOnce();
 });
